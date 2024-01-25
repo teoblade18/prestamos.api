b79db8af37a803e49b6fc45fd7ac10d4ccb4c4cd
T30 <mateo.a.e8@gmail.com>
2024-01-25 17:35:50 -0500

    guardar préstamo

--- prestamos.api/Controllers/PrestamoController.cs
+++ prestamos.api/Controllers/PrestamoController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using prestamos.api.Models;
@@ -14,6 +15,25 @@ namespace prestamos.api.Controllers
         public PrestamoController(ProyectoPrestamosContext _context)
         {
             _prestamosContext = _context;
+        }
+
+
+        [EnableCors("ReglasCors")]
+        [HttpPost]
+        [Route("Guardar")]
+        public IActionResult Guardar([FromBody] Prestamo objeto)
+        {
+            try
+            {
+                _prestamosContext.Prestamos.Add(objeto);
+                _prestamosContext.SaveChanges();
+
+                return StatusCode(StatusCodes.Status200OK, new { mensaje = "ok" });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, new { mensaje = ex.Message });
+            }
         }
 
     }
