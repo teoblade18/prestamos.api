5a5ab759d7db1751664912adcf90b251a6061cf1
T30 <mateo.a.e8@gmail.com>
2024-02-20 15:37:12 -0500

    crear y eliminar interes

--- prestamos.api/Controllers/InteresController.cs
+++ prestamos.api/Controllers/InteresController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using prestamos.api.Models;
@@ -14,6 +15,50 @@ namespace prestamos.api.Controllers
         public InteresController(ProyectoPrestamosContext _context)
         {
             _prestamosContext = _context;
+        }
+
+        [EnableCors("ReglasCors")]
+        [HttpPost]
+        [Route("Guardar")]
+        public IActionResult Guardar([FromBody] Interes objeto)
+        {
+            try
+            {
+
+                _prestamosContext.Intereses.Add(objeto);
+                _prestamosContext.SaveChanges();
+
+                return StatusCode(StatusCodes.Status200OK, new { mensaje = "ok" });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, new { mensaje = ex.Message });
+            }
+        }
+
+        [EnableCors("ReglasCors")]
+        [HttpDelete]
+        [Route("Eliminar/{idInteres}")]
+        public IActionResult Eliminar(int idInteres)
+        {
+            Interes oInteres = _prestamosContext.Intereses.Find(idInteres);
+
+            if (oInteres == null)
+            {
+                return BadRequest("Este interés no existe");
+            }
+
+            try
+            {
+                _prestamosContext.Intereses.Remove(oInteres);
+                _prestamosContext.SaveChanges();
+
+                return StatusCode(StatusCodes.Status200OK, new { mensaje = "ok" });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, new { mensaje = ex.Message });
+            }
         }
 
     }
