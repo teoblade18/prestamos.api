d3e10ff5721264e360becee153b763961ca671d4
T30 <mateo.a.e8@gmail.com>
2024-07-17 15:33:39 -0500

    Single responsability "Historial de préstamos" refactoring

--- prestamos.api/Controllers/PrestamoController.cs
+++ prestamos.api/Controllers/PrestamoController.cs
@@ -59,16 +59,9 @@ namespace prestamos.api.Controllers
         [Route("ConsultarPrestamosXPrestamista/{idPrestamista}")]
         public IActionResult ConsultarPrestamosXPrestamista(int idPrestamista)
         {
-            List<Prestamo> prestamos = new List<Prestamo>();
-
             try
             {
-                prestamos = _prestamosContext.Prestamos.Where(p => p.IdPrestamista == idPrestamista && (p.Estado == "Abonado" || p.Estado == "Impago"))
-                                                       .Include(c => c.oCliente)
-                                                       .Include(a => a.Abonos)
-                                                       .Include(i => i.Intereses)
-                                                       .OrderByDescending(p => p.Estado).ToList();
-
+                var prestamos = ObtenerPrestamos(idPrestamista, true);
                 return StatusCode(StatusCodes.Status200OK, new { mensaje = "ok", response = prestamos });
             }
             catch (Exception ex)
@@ -82,61 +75,85 @@ namespace prestamos.api.Controllers
         [Route("ConsultarHistorialPrestamos/{idPrestamista}")]
         public IActionResult ConsultarHistorialPrestamos(int idPrestamista)
         {
-            List<HistorialPrestamo> historialprestamos = new List<HistorialPrestamo>();
-
             try
             {
-                // Consultar los préstamos del prestamista incluyendo los abonos e intereses relacionados
-                var prestamos = _prestamosContext.Prestamos
-                    .Where(p => p.IdPrestamista == idPrestamista)
-                    .Include(c => c.oCliente)
-                    .Include(a => a.Abonos)
-                    .Include(i => i.Intereses)
-                    .OrderByDescending(p => p.Estado)
-                    .ToList();
-
-                // Calcular los totales y la deuda actual para cada préstamo
-                foreach (var prestamo in prestamos)
-                {
-                    int totalAbonos = (int)(prestamo.Abonos.Sum(a => a.Valor ?? 0));
-                    int totalIntereses = (int)(prestamo.Intereses.Sum(i => i.Valor ?? 0));
-                    int deudaActual = (int)(prestamo.MontoInicial + totalIntereses - totalAbonos);
-
-                    // Crear un objeto HistorialPrestamo con los datos del préstamo y los cálculos realizados
-                    var historialPrestamo = new HistorialPrestamo
-                    {
-                        IdPrestamo = prestamo.IdPrestamo,
-                        IdCliente = prestamo.IdCliente,
-                        IdPrestamista = prestamo.IdPrestamista,
-                        FechaInicial = prestamo.FechaInicial,
-                        FechaFinal = prestamo.FechaFinal,
-                        FechaProximoPago = prestamo.FechaProximoPago,
-                        Porcentaje = prestamo.Porcentaje,
-                        TipoIntereses = prestamo.TipoIntereses,
-                        DiaCorte = prestamo.DiaCorte,
-                        MontoInicial = prestamo.MontoInicial,
-                        MontoReal = prestamo.MontoReal,
-                        FechaPago = prestamo.FechaPago,
-                        Estado = prestamo.Estado,
-                        oCliente = prestamo.oCliente,
-                        oPrestamista = prestamo.oPrestamista,
-                        Intereses = prestamo.Intereses,
-                        Abonos = prestamo.Abonos,
-                        TotalAbonos = totalAbonos,
-                        TotalIntereses = totalIntereses,
-                        DeudaActual = deudaActual
-                    };
-
-                    // Agregar el objeto HistorialPrestamo a la lista
-                    historialprestamos.Add(historialPrestamo);
-                }
-
+                var prestamos = ObtenerPrestamos(idPrestamista, false);
+                var historialprestamos = CrearHistorialPrestamos(prestamos);
                 return StatusCode(StatusCodes.Status200OK, new { mensaje = "ok", response = historialprestamos });
             }
             catch (Exception ex)
             {
                 return StatusCode(StatusCodes.Status400BadRequest, new { mensaje = ex.Message });
             }
+        }
+
+        /// <summary>
+        /// Devuelve los préstamos presentes en la base de datos asociados a un prestamista específico.
+        /// En caso de que la consulta sea para el apartado "Prestamos" se buscan solo los activos
+        /// Si la consulta es para el apartado "Historial de prestamos" se entregan todos los préstamos.
+        /// </summary>
+        /// <param name="idPrestamista"> Id del prestamista que se desea filtrar.</param>
+        /// <param name="soloActivos"> Indica si se deben buscar solo los préstamos activos (true) o todos los préstamos (false).</param>
+        /// <returns>Lista de préstamos asociados a un prestamista.</returns>
+        private List<Prestamo> ObtenerPrestamos(int idPrestamista, bool soloActivos)
+        {
+            var prestamos = _prestamosContext.Prestamos
+                .Where(p => p.IdPrestamista == idPrestamista)
+                .Include(c => c.oCliente)
+                .Include(a => a.Abonos)
+                .Include(i => i.Intereses);
+
+            if (soloActivos)
+            {
+                return prestamos.Where(p => p.Estado == "Abonado" || p.Estado == "Impago").OrderByDescending(p => p.Estado).ToList();
+            }
+
+            return prestamos.OrderByDescending(p => p.Estado).ToList();
+        }
+
+        /// <summary>
+        /// Construye una lista de objetos HistorialPrestamo a partir de una lista de préstamos.
+        /// </summary>
+        /// <param name="prestamos">Lista de préstamos del prestamista.</param>
+        /// <returns>Lista de historial de préstamos con totales calculados y deuda actual.</returns>
+        private List<HistorialPrestamo> CrearHistorialPrestamos(List<Prestamo> prestamos)
+        {
+            var historialprestamos = new List<HistorialPrestamo>();
+
+            foreach (var prestamo in prestamos)
+            {
+                int totalAbonos = (int)(prestamo.Abonos.Sum(a => a.Valor ?? 0));
+                int totalIntereses = (int)(prestamo.Intereses.Sum(i => i.Valor ?? 0));
+                int deudaActual = (int)(prestamo.MontoInicial + totalIntereses - totalAbonos);
+
+                var historialPrestamo = new HistorialPrestamo
+                {
+                    IdPrestamo = prestamo.IdPrestamo,
+                    IdCliente = prestamo.IdCliente,
+                    IdPrestamista = prestamo.IdPrestamista,
+                    FechaInicial = prestamo.FechaInicial,
+                    FechaFinal = prestamo.FechaFinal,
+                    FechaProximoPago = prestamo.FechaProximoPago,
+                    Porcentaje = prestamo.Porcentaje,
+                    TipoIntereses = prestamo.TipoIntereses,
+                    DiaCorte = prestamo.DiaCorte,
+                    MontoInicial = prestamo.MontoInicial,
+                    MontoReal = prestamo.MontoReal,
+                    FechaPago = prestamo.FechaPago,
+                    Estado = prestamo.Estado,
+                    oCliente = prestamo.oCliente,
+                    oPrestamista = prestamo.oPrestamista,
+                    Intereses = prestamo.Intereses,
+                    Abonos = prestamo.Abonos,
+                    TotalAbonos = totalAbonos,
+                    TotalIntereses = totalIntereses,
+                    DeudaActual = deudaActual
+                };
+
+                historialprestamos.Add(historialPrestamo);
+            }
+
+            return historialprestamos;
         }
 
         [EnableCors("ReglasCors")]
